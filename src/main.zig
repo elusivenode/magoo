e46cb9fbeb38150c20103bb7c7bb5db1df3b1f55
Hamish MacDonald <elusivenode@gmail.com>
2026-08-12 06:06:34 +0000

    basic usage pattern
    add command "version"

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,15 @@
 const std = @import("std");
 
 pub fn main(init: std.process.Init) !void {
+    const version = 0.1;
     const args = try init.minimal.args.toSlice(init.arena.allocator());
     const n = args.len;
 
     if (n == 1) {
-        std.debug.print("usage: magoo args[1] {{more args}}\n", .{});
+        std.debug.print("usage: magoo <command>\n", .{});
     } else if (std.mem.eql(u8, args[1], "version")) {
-        std.debug.print("args[1] checked and is \"version\"\n", .{});
+        std.debug.print("magoo version {d}\n", .{version});
     } else {
-        std.debug.print("args[1] which was not \"version\"\n", .{});
+        std.debug.print("unknown command {s}\n", .{args[1]});
     }
 }
