8b74a47b25eeadaddb9f4381147e06c6b7da5bc6
Hamish MacDonald <elusivenode@gmail.com>
2026-08-12 05:59:46 +0000

    use std.mem.eql

--- src/main.zig
+++ src/main.zig
@@ -5,8 +5,10 @@ pub fn main(init: std.process.Init) !void {
     const n = args.len;
 
     if (n == 1) {
-        std.debug.print("magoo called with {d} args\n", .{n});
+        std.debug.print("usage: magoo args[1] {{more args}}\n", .{});
+    } else if (std.mem.eql(u8, args[1], "version")) {
+        std.debug.print("args[1] checked and is \"version\"\n", .{});
     } else {
-        std.debug.print("safe to check args[1] which was {s}\n", .{args[1]});
+        std.debug.print("args[1] which was not \"version\"\n", .{});
     }
 }
