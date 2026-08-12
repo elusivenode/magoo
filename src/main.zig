5f9c6d5fa344da8d6c44e0c5f744a97cf946dd0d
Hamish MacDonald <elusivenode@gmail.com>
2026-08-12 05:38:32 +0000

    args slice

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
-pub fn main() void {
-    std.debug.print("magoo\n", .{});
+pub fn main(init: std.process.Init) !void {
+    const args = try init.minimal.args.toSlice(init.arena.allocator());
+    const n = args.len;
+    std.debug.print("magoo called with {d} args\n", .{n});
 }
