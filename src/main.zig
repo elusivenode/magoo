05300778a015bcdb0dffbe04c7d880b753dec0b3
Hamish MacDonald <elusivenode@gmail.com>
2026-08-12 05:44:00 +0000

    using args[1] safely

--- src/main.zig
+++ src/main.zig
@@ -3,5 +3,10 @@ const std = @import("std");
 pub fn main(init: std.process.Init) !void {
     const args = try init.minimal.args.toSlice(init.arena.allocator());
     const n = args.len;
-    std.debug.print("magoo called with {d} args\n", .{n});
+
+    if (n == 1) {
+        std.debug.print("magoo called with {d} args\n", .{n});
+    } else {
+        std.debug.print("safe to check args[1] which was {s}\n", .{args[1]});
+    }
 }
